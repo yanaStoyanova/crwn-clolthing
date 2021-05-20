e406d76a771c0aca4f24cb3824ad10147bb0ed1f
Yana Stoyanova <y.stoyanova@blubito.com>
2021-05-20 13:51:26 +0300

    update url and router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => {
+  <div>
+  <h1>Hats page</h1></div>
+}
 
 function App() {
   return (
     <div>
-    <HomePage>
-    </HomePage>
+      <Switch>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route  path='/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
  
   );
